b4d4269bdf35d29bdb0d39bbad1204d90368ac5a
Joel Sjöström <joel.sjostrom@qbrick.com>
2018-02-23 14:46:12 +0100

    - font weights - presentation page

---
+++ src/components/presentation-page/PresentationPage.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './PresentationPage.css';
+
+class PresentationPage extends React.Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+
+
+  render () {
+    return (
+      <main className="presentation-page">
+      <div className="wrapper">
+        <h1>Arbetsprov</h1>
+          <p><strong>Val av ramverk</strong> föll initalt på <i>Angular 5</i> men jag blev med projektets gång allt mer intresserad av att testa <i>React</i> – så det fick bli en sådan version också. </p>
+       
+          <p><strong>Länkar</strong> till github och demosajter finns här nedan, Angular-versionen ligger ännu inte uppe men siktar på att få upp den live i helgen.</p>
+
+          <i>Joel Sjöström</i>
+
+          <section >
+              <h4>React</h4>
+              <ul>
+                  <li>
+                      <a href="https://github.com/beelio/new-salongen-react">Github</a>
+                  </li>
+                  <li>
+                      <a href="http://joelsjostrom.me/salonger">Demo</a>
+                  </li>
+              </ul>
+     
+              <h4>Angular 5</h4>
+              <ul>
+                  <li>
+                      <a href="https://github.com/beelio/salongen">Github</a>
+                  </li>
+                  <li>
+                      <a  href="">Demo</a> (Kommer snart)
+                  </li>
+              </ul>
+              <h4>Salong-api</h4>
+              <ul>
+                  <li>
+                      <a href="https://github.com/beelio/salongapi">Github</a>
+                  </li>
+              </ul>
+          </section>
+      </div>
+  </main>
+  
+    );
+  }
+}
+
+export default PresentationPage;
--- src/components/router-oulet/RouterOutlet.js
+++ src/components/router-oulet/RouterOutlet.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import SalonsPage from './../salons/SalonsPage';
 import SalonPage from './../salon/SalonPage';
+import PresentationPage from './../presentation-page/PresentationPage';
 
 class RouterOutlet extends Component {
   render () {
     return (
       <Switch>
+        <Route exact path='/' component={PresentationPage} />
         <Route path='/salonger' component={SalonsPage} />
         <Route path='/salong/:name/:id' component={SalonPage} />
       </Switch>
